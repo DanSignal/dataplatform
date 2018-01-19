59e850ecd12e7acaf7eb96980140075110f0cea7
DanSignal <335298960@qq.com>
2018-01-19 10:53:42 +0800

    Update prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://api.ibabygroup.cn"',
-  BASE_API2: '"http://api.ibabygroup.cn"'
+  BASE_API: '"http://apitest.ibabygroup.cn"',
+  BASE_API2: '"http://apitest.ibabygroup.cn"'
 }
