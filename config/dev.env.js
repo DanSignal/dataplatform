67970cdac91b70da5e78c2d7d4a013fb2ba09d73
DanSignal <335298960@qq.com>
2018-01-19 10:53:17 +0800

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://192.168.1.203:9696"',
-  BASE_API2: '"http://192.168.1.203:9696"'
+  BASE_API: '"http://apitest.ibabygroup.cn"',
+  BASE_API2: '"http://apitest.ibabygroup.cn"'
 })
